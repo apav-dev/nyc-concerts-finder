c5eb0e70049680136b172604d21ee2cf49a8d3cb
Aaron Pavlick <apavlick@yext.com>
2023-03-03 11:48:08 -0700

    visualize login request

--- functions/api/login.ts
+++ functions/api/login.ts
@@ -7,8 +7,6 @@ export const main = (argumentJson) => {
   const urlParams = new URLSearchParams(requestURL);
   const spotifyUrl = new URL(`https://accounts.spotify.com/authorize`);
 
-  // const purl = UrlLib.parse(requestURL);
-  // const redirect_uri = `${purl.protocol}//${purl.host}/callback`;
   const redirect_uri = "https://adoringly-alive-calf.pgsdemo.com/callback";
 
   let state = urlParams.get("state");
@@ -24,11 +22,43 @@ export const main = (argumentJson) => {
   spotifyUrl.searchParams.append("scope", scopes.join(" "));
   spotifyUrl.searchParams.append("state", state);
 
+  // return {
+  //   statusCode: 302,
+  //   headers: {
+  //     Location: spotifyUrl.toString(),
+  //   },
+  //   body: "",
+  // };
+  const argumentJsonToString = JSON.stringify(argumentJson);
+  const url = argumentJson["requestUrl"];
+  const userAgent = argumentJson["userAgent"];
+  let botscore = 0;
+  if (argumentJson["headers"]["X-Bot-Score"]) {
+    botscore = argumentJson["headers"]["X-Bot-Score"];
+  }
   return {
-    statusCode: 302,
-    headers: {
-      Location: spotifyUrl.toString(),
+    "body": `      
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            </head>
+            <body>
+                <div style="margin: auto; width:50%; border: 3px solid green; padding: 10px">
+                    <body>
+                        <h1>API!</h1>
+                        <div>Argument JSON: ${argumentJsonToString}</div>
+                        <div>Request URL: ${url}</div>
+                        <div>User Agent: ${userAgent}</div>
+                        <div>Bot Score: ${botscore}</div>
+                    </body>
+                </div>
+            </body>
+            </html>
+        `,
+    "statusCode": 200,
+    "headers": {
+      "Cache-control": "no-store",
+      "X-Yext-Test": "Example header",
     },
-    body: "",
   };
 };
